57dc01ee6366db9ebeb06d4389546e74338c4043
Ali Palasara <ali.wpsprintplan@gmail.com>
2024-07-02 17:26:10 +0530

    set the api key data.

--- admin/class.settings.php
+++ admin/class.settings.php
@@ -5,11 +5,13 @@ class WPSPI_Smart_Pipedrive_Admin_Settings {
        
         $client_id = $client_secret = "";
         
-       	if ( isset( $_POST['submit'] ) ) {
+        if ( isset( $_POST['submit'] ) ) {
                         
             $wpspi_smart_pipedrive_settings  = !empty(get_option( 'wpspi_smart_pipedrive_settings' )) ? get_option( 'wpspi_smart_pipedrive_settings' ) : array();
 
-            $wpspi_smart_pipedrive_settings = array_merge($wpspi_smart_pipedrive_settings, $_REQUEST['wpspi_smart_pipedrive_settings']);
+            if (isset($_REQUEST['wpspi_smart_pipedrive_settings'])) {
+                $wpspi_smart_pipedrive_settings = array_merge($wpspi_smart_pipedrive_settings, $_REQUEST['wpspi_smart_pipedrive_settings']);
+            }
             
             update_option( 'wpspi_smart_pipedrive_settings', $wpspi_smart_pipedrive_settings );
             
